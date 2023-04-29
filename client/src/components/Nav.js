dee3e24e3a027c7b95770902c8fb78a5013b5600
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-04-29 21:42:55 +0530

    Update logo

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,17 @@
-import whiteLogo from "../images/tinder_logo_white.png";
-import colorLogo from "../images/color-logo-tinder.png";
-
 const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
-  const handleClick = () => {
-    setShowModal(true);
-    setIsSignUp(false);
-  };
+    const handleClick = () => {
+        setShowModal(true);
+        setIsSignUp(false);
+    };
 
-  return (
-    <nav>
-      <div className="logo-container">
-        <img
-          className="logo"
-          src={minimal ? colorLogo : whiteLogo}
-          alt="logo"
-        />
-      </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div className="p-3 pl-5 text-4xl	">
+                <span className={minimal ? "text-black" : "text-white"}>
+                    AthleteMate
+                </span>
+            </div>
+        </nav>
+    );
 };
 export default Nav;
